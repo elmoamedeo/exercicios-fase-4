6c576258b7de17d5673b09eeac4fe1bf61417d0c
danielsimonebeira <dante_beira@hotmail.com>
2020-08-23 22:27:27 -0300

    Adiciona segunda parte do exercicio em vermelho

--- 12-08/exercicio-01.py
+++ 12-08/exercicio-01.py
@@ -4,7 +4,30 @@ class IdadePessoa:
             return print("Você é maior de idade!")
         return print("Você é menor de idade!")
 
+class NotaAluno:
+    def calcular_media(self, nota, qtd):
+        media_aluno = sum(nota) / qtd
+        return media_aluno
 
-idade = int(input("Digite a sua idade: "))
-idade_pessoa = IdadePessoa()
-idade_pessoa.calcular_idade(idade)
+    def verifica_aprovado(self, media):
+        if media >= 7:
+            print("Resultado: APROVADO")
+        else:
+            print("Resultado: REPROVADO")
+
+
+for conta_aluno in range(1, 5):
+    notas_soma = []
+    aluno = input("Digite o nome do aluno: ")
+    idade = int(input("Digite a sua idade: "))
+    idade_pessoa = IdadePessoa()
+    for conta_nota in range(1,4):
+        nota = float(input("Digite a nota {}: ".format(conta_nota)))
+        notas_soma.append(nota)
+    notaAluno = NotaAluno()
+    resultado_media = notaAluno.calcular_media(notas_soma, conta_nota)
+    print(50 * "=")
+    print("O aluno {} possui a nota média de {:.2f}. ".format(aluno, resultado_media))
+    idade_pessoa.calcular_idade(idade)
+    notaAluno.verifica_aprovado(resultado_media)
+    print(50 * "=")
--- 12-08/exercicio-04.py
+++ 12-08/exercicio-04.py
@@ -2,8 +2,28 @@ class Troco:
     def calcular_troco(self, x, y):
         print("O troco a ser dado é: R$" + str(x - y))
 
+    def troco_desconto(self, porcentagem, produto):
+        desconto = porcentagem / 100 * produto
+        print("Valor de desconto: R$ ", desconto)
+        novo_valor = produto - desconto
+        print('Novo Valor do produto: R$ ', novo_valor)
 
 valor_pago = float(input("Digite o valor pago: "))
 valor_produto = float(input("Digite o valor do produto: "))
-troco = Troco()
-troco.calcular_troco(valor_pago, valor_produto)
+
+
+while True:
+    valida_desconto_produto = input("Possui desconto: <S> ou <N>: ")
+    if valida_desconto_produto.upper() == 'S':
+        desconto_produto = float(input("Digite o desconto: "))
+        troco = Troco()
+        novo_valor_produto =troco.troco_desconto(desconto_produto, valor_produto)
+        troco.calcular_troco(valor_pago, novo_valor_produto)
+        break
+    elif valida_desconto_produto.upper() == 'N':
+        troco = Troco()
+        troco.calcular_troco(valor_pago, valor_produto)
+        break
+    else:
+        print(50 * "=","\nFavor, digitar S para Sim ou N para Não.")
+        print(50 * "=")
